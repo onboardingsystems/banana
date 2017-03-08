b356810b185e7b37574b4a2a9b4488d2eb981157
Joseph W. Lindley <josephl@tazworks.com>
2017-03-07 19:22:20 -0700

    Setup

---
+++ lib/banana.ex
@@ -0,0 +1,18 @@
+defmodule Banana do
+  @moduledoc """
+  Documentation for Banana.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Banana.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
